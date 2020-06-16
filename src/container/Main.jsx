3c8ea7bd4f8f2ccc76c4245868127e6f78ae615d
wally.olaondo <walter.olaondo@globallogic.com>
2020-06-16 17:16:53 -0300

    revert other change

--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -14,14 +14,6 @@ export default function Main() {
         >
           Learn React
         </a>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
       </header>
     </div>
   );
