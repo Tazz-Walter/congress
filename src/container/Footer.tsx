6ab6856e29c16584276dc59a3b7c6cf6fbfd806e
wally.olaondo <walter.olaondo@globallogic.com>
2020-06-18 11:51:08 -0300

    adding typescript

--- src/container/Footer.tsx
+++ src/container/Footer.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
+const styles = makeStyles((theme: Theme) => {
+  return createStyles({
     root: {
       flexGrow: 1,
     },
     footer: {
       backgroundColor: "coral",
-      padding: theme.spacing.unit * 2,
+      padding: theme.spacing(2),
       textAlign: "center",
     },
-  })
-);
+  });
+});
 
 export default function Footer() {
-  const classes = useStyles();
+  const classes = styles();
   return (
     <Grid item xs={12} className={classes.footer}>
       Congress wally Footer
